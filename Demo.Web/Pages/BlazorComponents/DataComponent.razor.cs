17d545a955dfbdb408d31248e2a5347507fc051b
Grant <Grant@DESKTOP-3O50M8K>
2022-06-19 20:04:19 +0100

    Add project files.

---
+++ Demo.Web/Pages/BlazorComponents/DataComponent.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorInCoreMVC.Pages.BlazorComponents
+{
+    public partial class DataComponent : ComponentBase
+    {
+
+        public List<DataItem> Items { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Items = new List<DataItem> { 
+                new DataItem { Name = "Test 1", Address = "Test 1"},
+                new DataItem { Name = "Test 2", Address = "Test 2"},
+            };
+        }
+
+
+    }
+}
---
+++ Demo.Web/Pages/BlazorComponents/DataItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorInCoreMVC.Pages.BlazorComponents
+{
+    public class DataItem
+    {
+        public string  Name { get; set; }
+        public string Address { get; set; }
+    }
+}
